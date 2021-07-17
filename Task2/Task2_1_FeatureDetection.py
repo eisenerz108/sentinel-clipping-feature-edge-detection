0665fc2128685dfada09cdee47ed8b97c7f174e2
Aman Jain <75158219+pyJain108@users.noreply.github.com>
2021-07-17 03:49:30 +0200

    Looping through all the writeImages and Plotting

--- Task2/Task2_1_FeatureDetection.py
+++ Task2/Task2_1_FeatureDetection.py
@@ -4,8 +4,8 @@ Created Date : 16-Jul-21
 """
 
 import cv2
-
 import util
+from matplotlib import pyplot as plt
 
 ## SIFT Algorithm
 
@@ -18,7 +18,7 @@ gray = util.readFile_gray()
 sift = cv2.SIFT_create(nfeatures=5000)
 keyPoints, descriptors = sift.detectAndCompute(image_sift, None)
 output_featuredet_sift = cv2.drawKeypoints(gray, keyPoints, image_sift, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-util.writeImage(output_featuredet_sift,'1_FD_SIFT')
+#util.writeImage(output_featuredet_sift,'1_FD_SIFT')
 
 ## ORB Algorithm
 
@@ -31,4 +31,17 @@ gray = util.readFile_gray()
 orb = cv2.ORB_create(nfeatures=100)
 keyPoints, descriptors = orb.detectAndCompute(image_orb, None)
 output_featuredet_orb = cv2.drawKeypoints(gray, keyPoints, image_orb, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-util.writeImage(output_featuredet_orb, '1_FD_ORB')
+#util.writeImage(output_featuredet_orb, '1_FD_ORB')
+
+titles = ['1_FD_GreyOriginalImage','1_FD_SIFT','1_FD_ORB']
+images = [gray, output_featuredet_orb, output_featuredet_sift]
+
+for title,image in zip(titles,images):
+    util.writeImage(image, title)
+
+for i in range(len(titles)):
+    plt.subplot(1, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
