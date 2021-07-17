ef2f8ab33f138ba8ffedc7fe5f5bf49aa594a579
Aman Jain <75158219+pyJain108@users.noreply.github.com>
2021-07-17 02:33:54 +0200

    Adding Task 3

--- Task2/Task2_2_EdgeDetection.py
+++ Task2/Task2_2_EdgeDetection.py
@@ -10,7 +10,7 @@ import numpy as np
 import util
 
 img = util.readFile_gray()
-img = cv2.GaussianBlur(img, (11,11), 0)
+#img = cv2.GaussianBlur(img, (11,11), 0)
 
 laplacian = cv2.Laplacian(img, cv2.CV_64F, ksize=5)
 
@@ -22,17 +22,13 @@ sobelY = cv2.Sobel(img, cv2.CV_64F, 0, 1)
 sobelY = np.uint8(np.absolute(sobelY))
 sobelCombined = cv2.bitwise_or(sobelX, sobelY)
 
-util.writeImage(img,'2_ED_GreyOriginalImage')
-util.writeImage(laplacian,'2_ED_Laplacian')
-util.writeImage(canny,'2_ED_Canny')
-util.writeImage(sobelX,'2_ED_SobelX')
-util.writeImage(sobelY,'2_ED_SobelY')
-util.writeImage(sobelCombined,'2_ED_SobelCombined')
-
-titles = ['Original Greyed Image','Laplacian','Canny','Sobel X','Sobel Y','Sobel Combined']
+titles = ['2_ED_GreyOriginalImage','2_ED_Laplacian','2_ED_Canny','2_ED_SobelX','2_ED_SobelY','2_ED_SobelCombined']
 images = [img, laplacian, canny, sobelX, sobelY, sobelCombined]
 
-for i in range(6):
+for title,image in zip(titles,images):
+    util.writeImage(image, title)
+
+for i in range(len(titles)):
     plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
---
+++ Task2/Task2_3_LowPassFilter.py
@@ -0,0 +1,31 @@
+"""
+Created Date : 17-Jul-21
+@author : Aman Jain
+"""
+
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import util
+
+img = util.readFile_coloured()
+kernel_5 = np.ones((5,5), np.float32)/25
+kernel_3 = np.ones((3,3), np.float32)/9
+
+con_image_5 = cv2.filter2D(img, -1, kernel_5)
+con_image_3 = cv2.filter2D(img, -1, kernel_3)
+blurImg = cv2.blur(img, (5,5))
+gblur = cv2.GaussianBlur(img, (5,5), 0)
+
+titles = ['3_LPF_OriginalImage','3_LPF_Convolved_3','3_LPF_Convolved_5','3_LPF_Blur','3_LPF_Gaussian Blur']
+images = [img, con_image_3, con_image_5, blurImg, gblur]
+
+for title,image in zip(titles,images):
+    util.writeImage(image, title)
+
+for i in range(len(titles)):
+    plt.subplot(1, 5, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
